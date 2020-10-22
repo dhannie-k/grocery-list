1e075a23643f59565a181591d353868428edc7bd
dhanniex82 <dhannie@live.com>
2020-10-22 14:16:19 +0700

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -102,11 +102,9 @@ class HomeScreen extends ConsumerWidget {
               child: PageView(
                 controller: _pageController,
                 children: pageViewItems,
-<<<<<<< HEAD
                 onPageChanged: (int index) =>
                     context.read(selectedIndex).state = index,
-=======
->>>>>>> de7038ea30dab370c28f1fb9cdd790ac3e6a8173
+
               ),
             )
           ],
